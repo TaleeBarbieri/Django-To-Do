6d99dce73729cb9aa9f28454ea222ba489e847bc
TaleeBarbieri <talee.alexei@hotmail.com>
2023-12-10 17:53:44 +0000

    added .gitignore

--- src/base/models.py
+++ src/base/models.py
@@ -7,7 +7,7 @@ class CustomUser(AbstractUser):
     first_name = models.CharField(_("first name"), max_length=150, blank=True)
     last_name = models.CharField(_("last name"), max_length=150, blank=True)
     email = models.EmailField(_("email address"), null=True, blank=True)
-    profile_picture = models.ImageField(upload_to='media/', null=True, blank=True)
+    profile_picture = models.ImageField(null=True, blank=True)
     last_login = models.DateTimeField(null=True, blank=True)
 
     groups = models.ManyToManyField(Group, blank=True)
